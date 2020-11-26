eeca4ce95d89ae7a72cef3eb3b79417b4163610b
Anderson Laurentino <anderson@laurentino.me>
2020-11-26 19:17:43 -0300

    Create services of Banners and Catalog && Modify the Banners and Ctalog Item

--- services/banner.go
+++ services/banner.go
@@ -1,7 +1,83 @@
 package services
 
-type bannerRepository struct{}
-
-var (
-	Banner bannerRepository = bannerRepository{}
+import (
+	"context"
+	"log"
+	"ng-catalog-service/models"
+	"ng-catalog-service/proto"
+	"ng-catalog-service/repositories"
 )
+
+type BannerService struct{ proto.BannerServiceServer }
+
+func (s *BannerService) Get(ctx context.Context, in *proto.Empty) (*proto.Banners, error) {
+	log.Println("[*] [BannerService] Get")
+
+	banners, err := repositories.Banner.Get()
+	if err != nil {
+		return nil, err
+	}
+
+	result := proto.Banners{}
+
+	for _, obj := range *banners {
+		result.Banners = append(result.Banners, obj.Proto())
+	}
+
+	return &result, nil
+}
+
+func (s *BannerService) GetById(ctx context.Context, in *proto.Id) (*proto.Banner, error) {
+	log.Println("[*] [BannerService] GetById")
+
+	catalog, err := repositories.Banner.GetById(in.Id)
+	if err != nil {
+		return nil, err
+	}
+
+	result := catalog.Proto()
+
+	return result, nil
+}
+
+func (s *BannerService) Insert(ctx context.Context, in *proto.Banner) (*proto.Status, error) {
+	log.Println("[*] [BannerService] Insert")
+
+	catalog := models.Banner{}
+	catalog.FromProto(in)
+
+	_, err := repositories.Banner.Insert(catalog)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &proto.Status{Code: 0, Message: "Ok"}, nil
+}
+
+func (s *BannerService) Update(ctx context.Context, in *proto.Banner) (*proto.Status, error) {
+	log.Println("[*] [BannerService] Update")
+
+	catalog := models.Banner{}
+	catalog.FromProto(in)
+
+	_, err := repositories.Banner.Update(catalog)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &proto.Status{Code: 0, Message: "Ok"}, nil
+}
+
+func (s *BannerService) Delete(ctx context.Context, in *proto.Id) (*proto.Status, error) {
+	log.Println("[*] [BannerService] Delete")
+
+	_, err := repositories.Banner.Delete(in.Id)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &proto.Status{}, nil
+}
--- services/catalog.go
+++ services/catalog.go
@@ -1,3 +1,83 @@
 package services
 
-type catalogRepository struct{}
+import (
+	"context"
+	"log"
+	"ng-catalog-service/models"
+	"ng-catalog-service/proto"
+	"ng-catalog-service/repositories"
+)
+
+type CatalogService struct{ proto.CatalogServiceServer }
+
+func (s *CatalogService) Get(ctx context.Context, in *proto.Empty) (*proto.Catalogs, error) {
+	log.Println("[*] [CatalogService] Get")
+
+	catalogs, err := repositories.Catalog.Get()
+	if err != nil {
+		return nil, err
+	}
+
+	result := proto.Catalogs{}
+
+	for _, obj := range *catalogs {
+		result.Catalogs = append(result.Catalogs, obj.Proto())
+	}
+
+	return &result, nil
+}
+
+func (s *CatalogService) GetById(ctx context.Context, in *proto.Id) (*proto.Catalog, error) {
+	log.Println("[*] [CatalogService] GetById")
+
+	catalog, err := repositories.Catalog.GetById(in.Id)
+	if err != nil {
+		return nil, err
+	}
+
+	result := catalog.Proto()
+
+	return result, nil
+}
+
+func (s *CatalogService) Insert(ctx context.Context, in *proto.Catalog) (*proto.Status, error) {
+	log.Println("[*] [CatalogService] Insert")
+
+	catalog := models.Catalog{}
+	catalog.FromProto(in)
+
+	_, err := repositories.Catalog.Insert(catalog)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &proto.Status{Code: 0, Message: "Ok"}, nil
+}
+
+func (s *CatalogService) Update(ctx context.Context, in *proto.Catalog) (*proto.Status, error) {
+	log.Println("[*] [CatalogService] Update")
+
+	catalog := models.Catalog{}
+	catalog.FromProto(in)
+
+	_, err := repositories.Catalog.Update(catalog)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &proto.Status{Code: 0, Message: "Ok"}, nil
+}
+
+func (s *CatalogService) Delete(ctx context.Context, in *proto.Id) (*proto.Status, error) {
+	log.Println("[*] [CatalogService] Delete")
+
+	_, err := repositories.Catalog.Delete(in.Id)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &proto.Status{}, nil
+}
